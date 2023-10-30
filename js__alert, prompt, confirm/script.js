d56ce2ce76a3111ab092b901e87e065c1529c063
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-10-30 12:04:20 +0300

    js

---
+++ js__alert, prompt, confirm/script.js
@@ -0,0 +1,12 @@
+// alert
+
+
+// prompt
+// result  = prompt("Возраст")  Запрашивает значение и добовляет в переменную
+
+
+// result = confrim("fdsfds");
+
+let isBoss = confirm("Ты здесь главный?");
+
+alert( isBoss ); // true, если нажата OK
